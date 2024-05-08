4e9e70826446272fa2c359a851555d413f200d58
Igor Gaidai <gaidai92@gmail.com>
2024-05-08 17:25:52 -0400

    Adds state generation

--- run_state_preparation.py
+++ run_state_preparation.py
@@ -1,11 +1,13 @@
 import os.path
 import pickle
+import random
 from functools import partial
 from multiprocessing import Pool
 
 import numpy as np
 import pandas as pd
 from qiskit import transpile, QuantumCircuit
+from qiskit.quantum_info import random_statevector, Statevector
 from tqdm import tqdm
 
 from src.quantum_walks import PathFinder, PathFinderLinear, PathFinderSHP, PathFinderMST
@@ -14,7 +16,7 @@ from src.walks_gates_conversion import PathConverter
 
 
 def prepare_state(target_state: dict[str, complex], method: str, path_finder: PathFinder, basis_gates: list[str], optimization_level: int, check_fidelity: bool = True,
-                  fidelity_tol: float = 1e-10, reduce_controls: bool = True) -> int:
+                  fidelity_tol: float = 1e-8, reduce_controls: bool = True) -> int:
     if method == "qiskit":
         target_state_vector = get_state_vector(target_state)
         num_qubits = len(next(iter(target_state.keys())))
@@ -32,9 +34,31 @@ def prepare_state(target_state: dict[str, complex], method: str, path_finder: Pa
         output_state_vector = execute_circuit(circuit_transpiled)
         target_state_vector = get_state_vector(target_state)
         fidelity = get_fidelity(output_state_vector, target_state_vector)
-        assert abs(1 - fidelity) < fidelity_tol, "Failed to prepare the state"
+        assert abs(1 - fidelity) < fidelity_tol, f"Failed to prepare the state. Fidelity: {fidelity}"
 
     return cx_count
+
+
+def generate_states():
+    num_qubits = np.array(list(range(3, 12)))
+    num_amplitudes = 2 ** (num_qubits - 1)
+    num_states = 1000
+
+    for n, m in zip(num_qubits, num_amplitudes):
+        out_path = f"data/qubits_{n}/m_{m}/states.pkl"
+        all_inds = list(range(2 ** n))
+        states = []
+        for i in range(num_states):
+            state_vector = random_statevector(len(all_inds)).data
+            zero_inds = random.sample(all_inds, len(all_inds) - m)
+            state_vector[zero_inds] = 0
+            state_vector /= sum(abs(amplitude) ** 2 for amplitude in state_vector) ** 0.5
+            state_dict = Statevector(state_vector).to_dict()
+            states.append(state_dict)
+
+        os.makedirs(os.path.dirname(out_path), exist_ok=True)
+        with open(out_path, "wb") as f:
+            pickle.dump(states, f)
 
 
 def run_prepare_state():
@@ -42,30 +66,31 @@ def run_prepare_state():
     # path_finder = PathFinderLinear()
     path_finder = PathFinderSHP()
     # path_finder = PathFinderMST()
-    basis_gates = ["rx", "ry", "rz", "h", "cx"]
-    optimization_level = 3
-    check_fidelity = True
-    reduce_controls = True
-    num_qubits_all = [3]
-    # num_qubits_all = [2, 3, 4, 5, 6, 7, 8, 9, 10]
-    out_col_name = "shp_reduced"
+    # num_qubits_all = np.array([11])
+    num_qubits_all = np.array(list(range(3, 12)))
+    num_amplitudes_all = 2 ** (num_qubits_all - 1)
+    out_col_name = "shp"
     num_workers = 20
+    reduce_controls = False
+    check_fidelity = True
+    optimization_level = 3
+    basis_gates = ["rx", "ry", "rz", "h", "cx"]
     process_func = partial(prepare_state, method=method, path_finder=path_finder, basis_gates=basis_gates, optimization_level=optimization_level, check_fidelity=check_fidelity,
                            reduce_controls=reduce_controls)
 
-    for num_qubits in num_qubits_all:
-        print(f"Num qubits: {num_qubits}")
-        results_path = f"data/qubits_{num_qubits}/cx_counts.csv"
-        results_folder = os.path.split(results_path)[0]
-        if not os.path.exists(results_folder):
-            os.makedirs(results_folder)
-        if os.path.isfile(results_path):
-            df = pd.read_csv(results_path)
+    for num_qubits, num_amplitudes in zip(num_qubits_all, num_amplitudes_all):
+        print(f"Num qubits: {num_qubits}; num amplitudes: {num_amplitudes}")
+        data_folder = f"data/qubits_{num_qubits}/m_{num_amplitudes}"
+        states_file_path = os.path.join(data_folder, "states.pkl")
+        with open(states_file_path, "rb") as f:
+            state_list = pickle.load(f)
+
+        cx_counts_file_path = os.path.join(data_folder, "cx_counts.csv")
+        if os.path.isfile(cx_counts_file_path):
+            df = pd.read_csv(cx_counts_file_path)
         else:
             df = pd.DataFrame()
 
-        with open(f"data/qubits_{num_qubits}/states.pkl", "rb") as f:
-            state_list = pickle.load(f)
         results = []
         if num_workers == 1:
             for result in tqdm(map(process_func, state_list), total=len(state_list), smoothing=0, ascii=' █'):
@@ -76,9 +101,10 @@ def run_prepare_state():
                     results.append(result)
 
         df[out_col_name] = results
-        df.to_csv(results_path, index=False)
+        df.to_csv(cx_counts_file_path, index=False)
         print(f"Avg CX: {np.mean(df[out_col_name])}\n")
 
 
 if __name__ == "__main__":
-    run_prepare_state()
+    generate_states()
+    # run_prepare_state()
