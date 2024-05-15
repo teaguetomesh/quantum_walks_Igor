77bb4856bc6e0151c696b407b6941133daaf7b88
Igor Gaidai <gaidai92@gmail.com>
2024-05-15 15:50:16 -0400

    Updates CX vs n plot

--- plots/quantum_walks.py
+++ plots/quantum_walks.py
@@ -4,6 +4,7 @@ from typing import Sequence
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+from matplotlib.lines import Line2D
 
 from plots.general import Line, plot_general, save_figure
 
@@ -15,26 +16,37 @@ def plot_cx_count_vs_num_qubits_line(method: str, num_qubits: Sequence[int], num
         df = pd.read_csv(data_path)
         data.append(np.mean(df[method]))
 
-    line = Line(num_qubits, data, color=color_ind, marker=marker_ind, label=label)
-    plot_general([line], ("n", "CX"), boundaries=(5, None, 10, 10 ** 4), figure_id=figure_id)
+    line = Line(num_qubits, data, color=color_ind, marker=marker_ind)
+    plot_general([line], label=label, figure_id=figure_id)
 
 
 def plot_cx_count_vs_num_qubits():
-    methods_all = [["shp_reduced", "qiskit", "shp"], ["shp_reduced", "qiskit"], ["shp_reduced", "qiskit"]]
-    labels_all = [["SHP", "Qiskit", "SHP w/o CR"], None, None]
-    num_qubits_all = [np.array(range(5, 12)), np.array(range(5, 12)), np.array(range(5, 10))]
-    num_amplitudes_all = [num_qubits_all[0], num_qubits_all[1] ** 2, 2 ** (num_qubits_all[2] - 1)]
+    methods_all = ["shp_reduced", "qiskit"]
+    densities_all = [lambda n: n, lambda n: n ** 2, lambda n: 2 ** (n - 1)]
     figure_id = 0
 
-    for density_ind in range(len(num_qubits_all)):
-        methods = methods_all[density_ind]
-        labels = labels_all[density_ind]
-        num_qubits = num_qubits_all[density_ind]
-        num_amplitudes = num_amplitudes_all[density_ind]
-        for method_ind, method in enumerate(methods):
-            label = labels[method_ind] if labels is not None else "_nolabel_"
-            plot_cx_count_vs_num_qubits_line(method, num_qubits, num_amplitudes, method_ind, density_ind, label, figure_id)
+    for method_ind, method in enumerate(methods_all):
+        for density_ind, density in enumerate(densities_all):
+            if method == "shp_reduced" and density_ind == 2:
+                num_qubits = np.array(range(5, 10))
+            else:
+                num_qubits = np.array(range(5, 12))
+            num_amplitudes = [densities_all[density_ind](n) for n in num_qubits]
+            plot_cx_count_vs_num_qubits_line(method, num_qubits, num_amplitudes, density_ind, method_ind, "_nolabel_", figure_id)
+
+    plt.xlabel = "n"
+    plt.ylabel = "CX"
+    plt.xlim(left=4.75)
+    plt.ylim(bottom=10, top=10 ** 4)
     plt.yscale("log")
+
+    circle_marker = Line2D([0], [0], linestyle="", color="k", marker="o", markersize=5, label="Quantum Walks")
+    star_marker = Line2D([0], [0], linestyle="", color="k", marker="*", markersize=8, label="Qiskit")
+    blue_line = Line2D([0], [0], color="b", label=r"$m = n$")
+    red_line = Line2D([0], [0], color="r", label=r"$m = n^2$")
+    green_line = Line2D([0], [0], color="g", label=r"$m = 2^{n-1}$")
+    plt.legend(handles=[circle_marker, star_marker, blue_line, red_line, green_line])
+
     save_figure()
 
 
