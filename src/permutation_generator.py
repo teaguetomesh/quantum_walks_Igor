02132e624c9c02c0f798bdc343dd4aa95a09bea1
Igor Gaidai <gaidai92@gmail.com>
2025-01-30 14:29:46 -0500

    Adds permutation generator based on hamming distnace

--- src/permutation_generator.py
+++ src/permutation_generator.py
@@ -1,5 +1,9 @@
 from abc import ABC, abstractmethod
 
+import networkx as nx
+import numpy as np
+from networkx.classes import Graph, DiGraph
+
 
 class PermutationGenerator(ABC):
     """ Generates state permutations. """
@@ -10,10 +14,29 @@ class PermutationGenerator(ABC):
         pass
 
 
-class PermutationGeneratorDense(PermutationGenerator):
-    """ Generates dense permutations. """
+class SequentialPermutator(PermutationGenerator):
+    """ Generates dense permutation in sequentially increasing order. """
 
     def get_permutation(self, state: dict[str, complex]) -> dict[str, str]:
-        """ Maps bases in the key iteration order to sequentially increasing bases. """
         num_qubits = len(next(iter(state)))
         return {basis: format(i, f'0{num_qubits}b') for i, basis in enumerate(state)}
+
+
+class MatchPermutator(PermutationGenerator):
+    """ Generates dense permutation based on Hamming distance. """
+
+    def get_permutation(self, state: dict[str, complex]) -> dict[str, str]:
+        num_qubits = len(next(iter(state)))
+        num_qubits_dense = int(np.ceil(np.log2(len(state))))
+        destinations = [format(i, f'0{num_qubits}b') for i in range(2 ** num_qubits_dense)]
+        match_graph = Graph()
+        for basis in state:
+            for destination in destinations:
+                distance = sum(b1 != b2 for b1, b2 in zip(basis, destination))
+                match_graph.add_edge(basis + '_o', destination + '_d', weight=distance)
+        pairs = list(nx.min_weight_matching(match_graph))
+        for i in range(len(pairs)):
+            if pairs[i][0][-1] == 'd':
+                pairs[i] = pairs[i][::-1]
+        permutation = {pair[0][:-2]: pair[1][:-2] for pair in pairs}
+        return permutation
