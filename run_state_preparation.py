10301dfbdb836ae0393ebced4c713cf4d16165e3
Igor Gaidai <gaidai92@gmail.com>
2025-05-12 13:35:46 -0400

    Updates data

--- run_state_preparation.py
+++ run_state_preparation.py
@@ -20,13 +20,13 @@ def prepare_state(target_state: dict[str, complex], circuit_generator: StateCirc
     circuit = circuit_generator.generate_circuit(target_state)
     circuit_transpiled = qiskit.transpile(circuit, **make_dict(basis_gates, optimization_level))
     circuit_transpiled = remove_leading_cx_gates(circuit_transpiled)
-    cx_count = circuit_transpiled.count_ops().get('cx', 0)
+    cx_count = circuit_transpiled.count_ops().get("cx", 0)
 
     if check_fidelity:
         output_state_vector = execute_circuit(circuit_transpiled)
         target_state_vector = get_state_vector(target_state)
         fidelity = get_fidelity(output_state_vector[:len(target_state_vector)], target_state_vector)
-        assert abs(1 - fidelity) < fidelity_tol, f'Failed to prepare the state. Fidelity: {fidelity}'
+        assert abs(1 - fidelity) < fidelity_tol, f"Failed to prepare the state. Fidelity: {fidelity}"
 
     return cx_count
 
@@ -41,31 +41,37 @@ def run_prepare_state():
 
     num_qubits = np.array([11])
     num_amplitudes = num_qubits ** 2
-    state_inds = list(range(0, 100))
-    out_col_name = 'mhs_multi'
+    # state_inds = list(range(0, 100))
+    state_inds = "-1"
+    out_col_name = "mhs_multi"
     # out_col_name = 'merging_states'
-    data_folder_parent = 'data'
+    data_folder_parent = "data"
     num_workers = 12
     check_fidelity = True
     optimization_level = 3
-    basis_gates = ['rx', 'ry', 'rz', 'h', 'cx']
+    basis_gates = ["rx", "ry", "rz", "h", "cx"]
     process_func = partial(prepare_state, **make_dict(circuit_generator, basis_gates, optimization_level, check_fidelity))
 
-    # iterable = list(product(num_qubits, num_amplitudes))
     for item in zip(num_qubits, num_amplitudes):
-        print(f'Current iterable: {item}')
-        data_folder = f'{data_folder_parent}/qubits_{item[0]}/m_{item[1]}'
-        states_file_path = os.path.join(data_folder, 'states.pkl')
-        with open(states_file_path, 'rb') as f:
+        print(f"Current iterable: {item}")
+        data_folder = f"{data_folder_parent}/qubits_{item[0]}/m_{item[1]}"
+        states_file_path = os.path.join(data_folder, "states.pkl")
+        cx_counts_file_path = os.path.join(data_folder, "cx_counts.csv")
+
+        with open(states_file_path, "rb") as f:
             state_list = pickle.load(f)
+
+        if state_inds == "-1":
+            df = pd.read_csv(cx_counts_file_path)
+            state_inds = df[out_col_name] == -1
         state_list = np.array(state_list)[state_inds]
 
         results = []
         if num_workers == 1:
-            for result in tqdm(map(process_func, state_list), total=len(state_list), smoothing=0, ascii=' █'):
+            for result in tqdm(map(process_func, state_list), total=len(state_list), smoothing=0, ascii=" █"):
                 results.append(result)
         else:
-            with tqdm(total=len(state_list), smoothing=0, ascii=' █') as progress_bar:
+            with tqdm(total=len(state_list), smoothing=0, ascii=" █") as progress_bar:
                 with ProcessPool(num_workers) as pool:
                     future_iter = pool.map(process_func, state_list, timeout=2 * 3600).result()
                     while True:
@@ -75,17 +81,17 @@ def run_prepare_state():
                             break
                         except TimeoutError:
                             results.append(0)
-                        except Exception:
+                        except Exception as e:
+                            print(e)
                             results.append(-1)
                         progress_bar.update()
 
-        cx_counts_file_path = os.path.join(data_folder, 'cx_counts.csv')
         df = pd.read_csv(cx_counts_file_path) if os.path.isfile(cx_counts_file_path) else pd.DataFrame()
         df.loc[state_inds, out_col_name] = results
         df.to_csv(cx_counts_file_path, index=False)
-        print(f'Avg CX: {np.mean(df[out_col_name])}\n')
+        print(f"Avg CX: {np.mean(df[out_col_name])}\n")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # np.random.seed(0)
     run_prepare_state()
